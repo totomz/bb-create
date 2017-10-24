bbcf61b7a1a7fe7f5f2fa5850e61806704fc679e
totomz <tommaso.doninelli@my-ideas.it>
2017-10-25 00:57:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# bb-create
-A small utility that generate Bitbucket repository given a configuration template!
+
+There are no tests here! Use this at your own risk!
+
+# Readme
+
+A tiny script to create `Bitbucket` repositories using a configuration file, instead of relying on manual configuration 
+
+Supported settings:
+* Creation of the repo with its metadata (name, description, private/public, etc.)
+* Create pipeline environment variables (but users must enable the pipeline manually)
+* Branch restrictions (number of review, number of passing builds)
+
+# How to use
+
+Install this module `npm install -g bb-create`
+
+Get an [App Password](https://confluence.atlassian.com/bitbucket/app-passwords-828781300.html) and write a `~/.bbcreate-settings.json` config file
+```
+{
+    "username": <YOUR_USER_NAME>,
+    "appPassword": <YES_EXACTLY_WHAT_HE_SAYS> 
+}
+```
+
+Then write the definition of your Bitbucket repo in a JSON file. 
+
+Last step, run `bb-create <REPO_DEFINITION>.json`
+
+
+## How to define a Bitbucket repository
+See `example-repo.json` and refer to the Atlassian documentaiton 
+ * [Repository](https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D)
+ * [Pipeline configuration](https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/pipelines_config)
+ * [BRanch restrictions](https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/branch-restrictions)
---
+++ index.js
@@ -0,0 +1,86 @@
+const merge = require('deepmerge');
+const rp = require('request-promise');
+const Promise = require("bluebird");
+const fs = require('fs');
+const path = require('path');
+
+
+
+function BBCreate(settings, repoDef){
+    this.settings = settings;
+    this.repoDef = repoDef;
+}
+
+/**
+ *
+ * @param repoName {string} "team/repoName'
+ */
+BBCreate.prototype.createRepo = function() {
+
+    const repoName = this.repoDef.repoName;
+    const repoSettings = this.repoDef.repoSettings;
+    const pipelineEnvs = this.repoDef.pipelineEnvs;
+    const branchRestrictions = this.repoDef.branchRestrictions;
+    const me = this;
+
+    return this.buildRequest(`repositories/${repoName}`, {
+        method: 'POST',
+        body: repoSettings
+    })
+    /* Create the pipelines variables */
+    .then(repo => {
+        console.log(`Repo created - ${repo.uuid}. `)
+        let requests = [];
+        pipelineEnvs.forEach(env => {
+            console.log(`Creating env ${env.key}`);
+            requests.push(this.buildRequest(`repositories/${repoName}/pipelines_config/variables/`, {
+                method: 'POST',
+                body: env
+            }))
+        });
+
+        return Promise.all(requests);
+    })
+    /* Create the branch restirctions */
+    .then(envs => {
+        let requests = [];
+        branchRestrictions.forEach(restriction => {
+            console.log(`Creating restriction ${restriction.kind}`);
+            requests.push(this.buildRequest(`repositories/${repoName}/branch-restrictions`, {
+                method: 'POST',
+                body: restriction
+            }))
+        });
+
+        return Promise.all(requests);
+    })
+};
+
+BBCreate.prototype.buildRequest = function(path, options){
+    const params = merge({
+        url: `https://api.bitbucket.org/2.0/${path}`,
+        json:true,
+        auth: {
+            'user': this.settings.username,
+            'pass': this.settings.appPassword,
+            'sendImmediately': true
+        }
+    }, (options || {}));
+
+    return rp(params);
+};
+
+const home = process.env.HOME || process.env.USERPROFILE;
+const settings = path.join(home, '.bbcreate-settings.json');
+const repoOpt = path.resolve(process.argv.pop());
+
+console.log(`Reading settings from ${settings}`);
+console.log(`Reading repo definition from ${repoOpt}`);
+
+new BBCreate(
+    JSON.parse(fs.readFileSync(settings, 'UTF-8')),
+    JSON.parse(fs.readFileSync(repoOpt, 'UTF-8'))
+)
+.createRepo()
+.then(repo => { console.log("DONE"); })
+.catch(err => { console.log("ERROR!", err); });
